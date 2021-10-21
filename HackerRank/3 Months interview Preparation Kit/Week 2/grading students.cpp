cd99f40d1a7f4b899fc63bc7d3a6dff715ce19d1
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2021-10-21 06:01:50 -0700

    Problem #11

---
+++ HackerRank/3 Months interview Preparation Kit/Week 2/grading students.cpp
@@ -0,0 +1,10 @@
+vector<int> gradingStudents(vector<int> grades) {
+    for(int i = 0 ; i < grades.size() ; i++)
+        {
+            if (grades[i] < 38) continue;
+            else {
+                if ((5-(grades[i]%5)) < 3 ) grades[i] = grades[i] + (5-(grades[i]%5));
+            }
+        }
+        return grades;
+}
