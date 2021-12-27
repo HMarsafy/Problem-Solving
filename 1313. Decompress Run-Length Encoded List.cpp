5541ecaa521c426e4f0292cb83b38a37372619e2
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2021-12-27 18:44:34 +0200

    Problem #16

---
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,13 @@
+ vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0 ; i<nums.size()/2 ; i++)
+        {
+            int freq = nums[2*i];
+            int val = nums [2*i+1];
+            for(int j = 0 ; j < freq ; j++)
+            {
+                ans.push_back(val);
+            }
+        }
+        return ans;
+    }
