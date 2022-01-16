39b92af7c8bfb8fac359e1ce8344bf6288282fc4
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-01-16 19:07:05 +0200

    Problem #27

---
+++ LeetCode/Easy/1662. Check If Two String Arrays are Equivalent.cpp
@@ -0,0 +1,13 @@
+   bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string first =""; string second="";
+        for(int i = 0 ; i < word1.size() ; i++)
+        {
+            first+=word1[i];
+        }
+        for(int i = 0 ; i < word2.size() ; i++)
+        {
+            second+=word2[i];
+        }
+        if(first == second) return 1;
+        return 0;
+    }
