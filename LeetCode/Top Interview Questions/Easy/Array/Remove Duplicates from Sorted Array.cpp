23bc34aaec7a47ace334a92f06edfa3f43403bc4
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-01-23 10:02:12 +0200

    Problem #43

---
+++ LeetCode/Top Interview Questions/Easy/Array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,14 @@
+int removeDuplicates(vector<int>& nums) 
+        {
+            int index = 0 ;
+            if(nums.size() == 0) return 0;
+            for(int i = 0 ; i < nums.size()-1 ; i++)
+            {
+                if(nums[i] != nums[i+1])
+                {
+                    nums[index++] = nums[i];
+                }
+            }
+        nums[index++] = nums[nums.size()-1];
+        return index;
+        }
