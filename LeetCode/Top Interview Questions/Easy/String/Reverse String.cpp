e52d363ae6e0a1c4c6990efe74f1ee589b3ff835
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-01-26 07:33:26 +0200

    Problem #46

--- LeetCode/Top Interview Questions/Easy/String/Reverse String.cpp
+++ LeetCode/Top Interview Questions/Easy/String/Reverse String.cpp
@@ -9,3 +9,16 @@
             s[i] = w[i];
         }
     }
+/////////////////////////////////////// ANOTHER SOLUTION ////////////////////////////////////////
+
+ void reverseString(vector<char>& s) {
+    int j = 0 ;
+    int i = s.size()-1;
+    char temp;
+    for(int itr = 0 ; itr<s.size()/2 ; itr++)
+    {
+        temp = s[j];
+        s[j++] = s[i];
+        s[i--] = temp;
+    }
+    }
