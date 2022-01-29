294fcf2187e3e89941140c4aecebd94ab0e6b5ce
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-01-29 08:07:11 +0200

    Problem #52

--- LeetCode/Top Interview Questions/Easy/String/242. Valid Anagram.cpp
+++ LeetCode/Top Interview Questions/Easy/String/242. Valid Anagram.cpp
@@ -1,10 +1,23 @@
-    bool isAnagram(string s, string t) {
+   bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return 0;
+        vector<int> freq(26,0);
+        for(int i =0 ; i<s.size() ; i++)
+            freq[(int)s[i] - 'a']++;
+        for(int i = 0 ;  i<t.size() ; i++)
+        {
+            int index = (int)t[i] - 'a';
+            if(freq[index] == 0) return 0;
+            freq[index]--;
+        }
+        return 1;
+        
+///////////////////////////////////////////////////////////////// ANOTHER SOLUTION ///////////////////////////////////////////////////
+        /*if(s.size() != t.size()) return 0;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         for(int i = 0; i<s.size() ; i++)
         {
             if(s[i] != t[i]) return 0;
         }
-        return 1;
+        return 1;*/
     }
