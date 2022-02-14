91005b6f156b8d6ef1e72fedf8350b1f7f349dd8
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-02-14 10:49:30 +0200

    Problem #71

---
+++ LeetCode/Easy/2169. Count Operations to Obtain Zero.cpp
@@ -0,0 +1,16 @@
+   int countOperations(int num1, int num2) {
+        int num = 0;
+        while(num1 !=0 && num2!= 0)
+        {
+            if(num1 >= num2)
+            {
+                num1 = num1 - num2;
+            }
+            else
+            {
+                num2 = num2 - num1;
+            }
+            num++;
+        }
+        return num;
+    }
