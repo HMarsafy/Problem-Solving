0670774f438cc7cd9081a0a418ce3d0dba460484
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2022-10-10 22:36:00 +0200

    Problem #33

---
+++ Solved Again/LeetCode/1913. Maximum Product Difference Between Two Pairs.cpp
@@ -0,0 +1,5 @@
+ int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        int i = nums.size() - 1 ;
+        return (nums[i] * nums[i-1])-(nums[0]*nums[1]);
+    }
